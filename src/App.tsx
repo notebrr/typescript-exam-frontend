c04b0c28614aa75df11bd212848e058a572c6908
R <90389727+notebrr@users.noreply.github.com>
2023-03-01 09:32:23 +0100

    Styled and set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import myLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
@@ -8,25 +8,15 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <a href="/">
+              <img src={myLogo} className="app logo" alt="App logo" />
+          </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Movie app</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          <input type="text" className={"searchField"} autoFocus={true} placeholder={"Search movie"}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
